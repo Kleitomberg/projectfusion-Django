6ec6fe2a912874e32cfaded38fed983723574617
kleitomberg <ozkberguinho@gmail.com>
2021-11-29 19:28:14 -0300

    lte2

--- fusion/settings.py
+++ fusion/settings.py
@@ -33,13 +33,16 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
+    'django_adminlte',
+    'django_adminlte_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    
 ]
 
 MIDDLEWARE = [
